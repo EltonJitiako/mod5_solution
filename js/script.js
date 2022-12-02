4f20781c41a94577459ca72db9bab0d5fd2309f3
Elton Jitiako <eltonjitiako@gmail.com>
2022-12-02 19:48:35 +0100

    c2

--- js/script.js
+++ js/script.js
@@ -153,7 +153,7 @@ dc.loadMenuItems = function (categoryShort) {
   showLoading("#main-content");
   $ajaxUtils.sendGetRequest(
     menuItemsUrl + categoryShort + ".json",
-    buildAndShowMenuItemsHTML);
+    buildAndShowHomeHTML);
 };
 
 
